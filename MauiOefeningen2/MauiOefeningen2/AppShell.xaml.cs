172feba63f2a7df5fc1f168ad97ad61f322d3006
PJPRORL <piussi-jeroen@hotmail.com>
2026-04-19 10:07:48 +0200

    Update van PersoonPage, PersoonViewModel en Persoon.cs. Update van MainPage en AppShell

---
+++ MauiOefeningen2/MauiOefeningen2/AppShell.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace MauiOefeningen2
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+
+            //Routing.RegisterRoute(nameof(PersoonPage), typeof(PersoonPage));
+            Routing.RegisterRoute(nameof(PersoonDetailPage), typeof(PersoonDetailPage));
+        }
+    }
+}
